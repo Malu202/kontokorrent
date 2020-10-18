e8ebfa15007c7eb4be826e4768184444cd4f1328
Raphael <raphael.hauk@racing.tuwien.ac.at>
2020-10-18 15:38:58 +0200

    fix bezahlung eintragen overflow

--- reducerstuff.ts
+++ reducerstuff.ts
@@ -1,47 +1,43 @@
-interface State{
-    slice1:string;
-    slice2:string;
-  }
-  
-  interface Reducer<T extends keyof State, A extends SliceMap<T>> {
-  }
-  
-  class BaseReducer<T extends keyof State, A extends SliceMap<T>> implements Reducer<T,A> {
-  }
-  
-  type ReducerCtor<T  extends keyof State> = { new() :Reducer<T, any> };
-  
-  class Action {
-      readonly sliceMap = Reducer1.sliceMap;
-  }
-  let x = new Action();
-  
-  class Action2 {
-      readonly sliceMap = {
-        ...Reducer1.sliceMap,
-        ...Reducer2.sliceMap
-      };
-      constructor() {
-  
-      }
-  }
-  
-  class Reducer1 extends BaseReducer<"slice1", Action | Action2>  {
-    static sliceMap = {
-      "slice1" : Reducer1
-    }
-  }
-  
-  class Reducer2 extends BaseReducer<"slice2", Action2>  {
-    static sliceMap = {
-      "slice2" : Reducer2
-    }
-  }
-  
-  type SliceMap<E extends keyof State> = {
-    sliceMap: {
-      [D in E] : ReducerCtor<E>
-    }
+interface State {
+  slice1: string;
+  slice2: string;
+}
+
+interface Reducer<T extends keyof State, A extends SliceMap<T>> {
+}
+
+class BaseReducer<T extends keyof State, A extends SliceMap<T>> implements Reducer<T, A> {
+}
+
+type ReducerCtor<T extends keyof State> = { new(): Reducer<T, any> };
+
+class Action {
+  readonly sliceMap = {
+    "slice1": Reducer1
   };
-  
-  
+}
+
+class Action2 {
+  readonly sliceMap = {
+    "slice2": Reducer2,
+    "slice1": Reducer1
+  }
+  constructor() {
+
+  }
+}
+
+class Reducer1 extends BaseReducer<"slice1", Action | Action2>  {
+
+}
+
+class Reducer2 extends BaseReducer<"slice2", Action2>  {
+
+}
+
+type SliceMap<E extends keyof State> = {
+  sliceMap: {
+    [D in E]: ReducerCtor<E>
+  }
+};
+
